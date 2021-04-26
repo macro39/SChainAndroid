7bfc16991f445c4721ddda8a77fb148668dee3db
Kamil Macek <milo.macek@gmail.com>
2021-04-26 15:49:24 +0200

    themes and colors corrections, logo added

--- app/src/main/java/zigzaggroup/schain/mobile/SchainApplication.kt
+++ app/src/main/java/zigzaggroup/schain/mobile/SchainApplication.kt
@@ -1,7 +1,6 @@
 package zigzaggroup.schain.mobile
 
 import android.app.Application
-import com.crazylegend.kotlinextensions.misc.disableNightMode
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -10,7 +9,6 @@ class SchainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        disableNightMode()  // TODO remove after dark theme optimization
         Timber.plant(Timber.DebugTree())
     }
 
